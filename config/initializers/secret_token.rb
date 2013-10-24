c2efe3f0815747895053d01a8a4d36169b8b44c2
Jeff Barriault <barriauj@tamug.edu>
2013-10-24 13:17:43 -0500

    initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+CompassUp::Application.config.secret_token = '5eff1d77a8cf0c565098ede05a82f3433d04873b0f27161f3435d62caa46c0ba732e38d7d9049e0a2e4d6476a90b0bd5eb2bfae429e8138c5f92e3a1028873b2'
