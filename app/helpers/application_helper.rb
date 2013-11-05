eb33a8373ebd66b760d94223a02ef748e553b66c
Jeff Barriault <barriauj@tamug.edu>
2013-11-05 13:13:52 -0600

    cleanup nav tabs

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -8,4 +8,12 @@ module ApplicationHelper
     options[:html] = options[:html] || { class: "form-horizontal", role: "form" } 
     form_for(object, options, &block)
   end
+  
+  def nav_tab_link_to(path, label, icon, options = {})
+    content_tag(:li, options) do
+      link_to(path) do
+        content_tag(:span, nil, class: "glyphicon #{icon}") + " #{label}"
+      end
+    end
+  end
 end
